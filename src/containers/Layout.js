98567cea6156f81a9b4b5ff77cd728d683e30473
poddarrishi02 <poddarrishi02@gmail.com>
2022-01-13 21:26:00 +0530

    Module 2 completed(mostly)

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -3,19 +3,24 @@ import Navbar from "../Components/LandingPage/Navbar/Navbar";
 import Footer from "./Footer";
 import classes from "./layout.module.css";
 import { AuthContext } from "../store/auth-context";
+import ProtectedRoute from "../store/ProtectedRoute";
 export default function Layout(props) {
   const AuthCtx = React.useContext(AuthContext);
   return (
     <div>
       <div className={classes.container}>
         <div className={classes.header}>
-          <Navbar />
+          {/* <ProtectedRoute> */}
+            <Navbar />
+          {/* </ProtectedRoute> */}
         </div>
         {props.children}
       </div>
       {AuthCtx.isLoggedIn && (
         <div className={classes.footer}>
-          <Footer />
+          {/* <ProtectedRoute> */}
+            <Footer />
+          {/* </ProtectedRoute> */}
         </div>
       )}
     </div>
